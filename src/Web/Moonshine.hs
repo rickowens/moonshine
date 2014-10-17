405f64180d04470ec466396aee6e8aa4fcf0aeca
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-10-17 19:11:15 -0400

    Moonshine: introduce LoggingConfig
    Developers are expected to parse a LoggingConfig somewhere out of their
    config. Moonshine will use this LoggingConfig to initialize logging
    configuration.

--- src/Web/Moonshine.hs
+++ src/Web/Moonshine.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Web.Moonshine (
+  LoggingConfig(), HasLoggingConfig(..),
   runMoonshine,
   route
 ) where
 
+import GHC.Generics (Generic)
+
+import qualified Data.Text as T
 import Data.ByteString (ByteString)
 import System.Directory (createDirectoryIfMissing)
 import Snap (Snap, quickHttpServe, MonadSnap)
-import Data.Yaml (FromJSON, decodeFileEither)
+import Data.Yaml (FromJSON(..), decodeFileEither)
+import Data.Aeson (Value(..))
+import System.Log (Priority())
 import qualified Snap (route)
 
 -- Public Types ---------------------------------------------------------------
@@ -16,11 +23,12 @@ import qualified Snap (route)
 {- |
   Run a snap web service in the moonshine framework.
 -}
-runMoonshine :: FromJSON a => (a -> Snap ()) -> IO ()
+runMoonshine :: (FromJSON a, HasLoggingConfig a) => (a -> Snap ()) -> IO ()
 runMoonshine init = do
   createDirectoryIfMissing True "log"
   let configPath = "config.yml"
   config <- loadConfig configPath
+  whenMaybe (getLoggingConfig config) setupLogging
   quickHttpServe (init config)
 
 
@@ -40,5 +48,34 @@ loadConfig path = do
 route :: MonadSnap m => [(ByteString, m a)] -> m a
 route = Snap.route
 
+data LoggingConfig = LoggingConfig {
+  level :: Priority
+} deriving (Generic)
+
+class HasLoggingConfig a where
+  getLoggingConfig :: a -> Maybe LoggingConfig
+  getLoggingConfig _ = Nothing
+
+instance FromJSON LoggingConfig
+instance FromJSON Priority where
+  parseJSON (String s) = case reads (T.unpack s) of
+    [(priority, "")] -> return priority
+    _ -> fail $ "couldn't parse Priority from string " ++ show s
+  parseJSON value = fail $ "Couldn't parse Priority from value " ++ show value
+
 -- Private Types --------------------------------------------------------------
 -- Private Functions ----------------------------------------------------------
+
+{- |
+  Conditionally execute an action depending on the value of the Maybe.
+  This is like the "when" from Control.Monad, combined with pattern matching.
+-}
+whenMaybe :: Maybe a -> (a -> IO ()) -> IO ()
+whenMaybe Nothing _= return ()
+whenMaybe (Just a) f = f a
+
+{- |
+  FIXME
+-}
+setupLogging :: LoggingConfig -> IO ()
+setupLogging loggingConfig = putStrLn "FIXME: setting up logging somehow"
