c9a994be54eabf8896ac5217cb106ed6a8322c65
Rick Owens <rowens@sumall.com>
2014-10-17 13:19:42 -0400

    Added `runMoonshine`, and `route`, which still needs implementation.

--- src/Web/Moonshine.hs
+++ src/Web/Moonshine.hs
@@ -1,9 +1,27 @@
 module Web.Moonshine (
+  runMoonshine
 ) where
+
+import Data.ByteString (ByteString)
+import Snap (Snap, quickHttpServe, MonadSnap)
 
 -- Public Types ---------------------------------------------------------------
 -- Semi-Public Types ----------------------------------------------------------
 -- Public Functions -----------------------------------------------------------
+
+{- |
+  Run a snap web service in the moonshine framework.
+-}
+runMoonshine :: Snap () -> IO ()
+runMoonshine = quickHttpServe
+
+
+{- |
+  A version of `Snap.route` that autmatically sets up metrics for the specified routs.
+-}
+route :: MonadSnap m => [(ByteString, m a)] -> m a
+route = error "implement me"
+
 -- Private Types --------------------------------------------------------------
 -- Private Functions ----------------------------------------------------------
 
