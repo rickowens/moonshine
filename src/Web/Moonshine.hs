6ca96918d1f7f81f3e1b6e29e737682562aae25b
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-10-17 17:55:14 -0400

    read config from hardcoded filename

--- src/Web/Moonshine.hs
+++ src/Web/Moonshine.hs
@@ -5,6 +5,7 @@ module Web.Moonshine (
 
 import Data.ByteString (ByteString)
 import Snap (Snap, quickHttpServe, MonadSnap)
+import Data.Yaml (FromJSON, decodeFileEither)
 import qualified Snap (route)
 
 -- Public Types ---------------------------------------------------------------
@@ -14,11 +15,22 @@ import qualified Snap (route)
 {- |
   Run a snap web service in the moonshine framework.
 -}
-runMoonshine :: (a -> Snap ()) -> IO ()
-runMoonshine init =
-  let config = undefined in
+runMoonshine :: FromJSON a => (a -> Snap ()) -> IO ()
+runMoonshine init = do
+  let configPath = "config.yml"
+  config <- loadConfig configPath
   quickHttpServe (init config)
 
+
+{- |
+  Load the configuration from YAML.
+-}
+loadConfig :: FromJSON a => FilePath -> IO a
+loadConfig path = do
+  eConfig <- decodeFileEither path
+  case eConfig of
+    Left errorMsg -> error $ "Couldn't decode YAML config from file " ++ path ++ ": " ++ (show errorMsg)
+    Right config -> return config
 
 {- |
   A version of `Snap.route` that autmatically sets up metrics for the specified routs.
